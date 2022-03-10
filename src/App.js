82414c574529c6b1ca807d308dd32bbff16aa021
Eula Bengco <eulabengco@gmail.com>
2022-03-09 21:25:10 -0500

    install material-ui and add button component in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          startIcon={<SaveIcon />}
+          size="large"
+          style={{ fontSize: 20 }}
+          variant="contained"
+          color="secondary"
+          href="#"
         >
-          Learn React
-        </a>
+          Hello World
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
