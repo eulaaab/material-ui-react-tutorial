70db0eec9b2eb39002a43591a0895c69fbd5ff48
Eula Bengco <eulabengco@gmail.com>
2022-03-09 21:37:13 -0500

    add discard button and checkbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { ButtonGroup } from "@material-ui/core";
+import Checkbox from "@material-ui/core/Checkbox";
 
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <div>
+      <Checkbox
+        checked={checked}
+        onChange={(e) => setChecked(e.target.value)}
+      />
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          startIcon={<SaveIcon />}
-          size="large"
-          style={{ fontSize: 20 }}
-          variant="contained"
-          color="secondary"
-          href="#"
-        >
-          Hello World
-        </Button>
+        <CheckboxExample />
+        <ButtonGroup variant="contained" color="primary">
+          <Button
+            startIcon={<SaveIcon />}
+            size="large"
+            style={{ fontSize: 20 }}
+            href="#"
+          >
+            Save
+          </Button>
+          <Button
+            startIcon={<DeleteIcon />}
+            size="large"
+            style={{ fontSize: 20, marginLeft: 10 }}
+            href="#"
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
