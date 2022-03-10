6cf0202ff0bdba14fa0cc4246ddddaf7b231484b
Eula Bengco <eulabengco@gmail.com>
2022-03-09 23:17:49 -0500

    import roboto font, update md, and import appbar, toolbar, and icon button for header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
-import { ButtonGroup } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import { AppBar, ButtonGroup, Toolbar, IconButton } from "@material-ui/core";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from "@material-ui/core/TextField";
@@ -78,66 +78,75 @@ function CheckboxExample() {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="small">
-        <div className="App">
-          <header className="App-header">
-            {/* making it  div component */}
-            <Typography variant="h2" component="div">
-              Material UI Tutorial
-            </Typography>
-            <Typography variant="h6">Brushing up on Material UI</Typography>
-            <ButtonStyle />
-            {/* grid can be a container or an item */}
-            <Grid container spacing={4} justify="center">
-              <Grid item xs={3} sm={6}>
-                {/* i.e. card */}
-                <Paper style={{ height: 75, width: "100%" }} />
-              </Grid>
-              <Grid item xs={3} sm={6}>
-                {/* i.e. card */}
-                <Paper style={{ height: 75, width: "100%" }} />
-              </Grid>
-              <Grid item xs={3} sm={6}>
-                {/* i.e. card */}
-                <Paper style={{ height: 75, width: "100%" }} />
-              </Grid>
+      {/* <Container maxWidth="xs"> */}
+      {/* <Container> */}
+      <div className="App">
+        <header className="App-header">
+          <AppBar>
+            <Toolbar>
+              <IconButton>
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="subtitle">MUI Theming</Typography>
+              <Button>Login</Button>
+            </Toolbar>
+          </AppBar>
+          {/* making it  div component */}
+          <Typography variant="h2" component="div">
+            Material UI Tutorial
+          </Typography>
+          <Typography variant="h6">Brushing up on Material UI</Typography>
+          <ButtonStyle />
+          {/* grid can be a container or an item */}
+          <Grid container spacing={4} justify="center">
+            <Grid item xs={3} sm={6}>
+              {/* i.e. card */}
+              <Paper style={{ height: 75, width: "100%" }} />
             </Grid>
-            <TextField
-              variant="outlined"
-              color="secondary"
-              placeholder="test@test.com"
-              label="email"
-            />
-            <TextField variant="filled" color="secondary" type="date" />
-            <TextField
-              variant="outlined"
-              color="secondary"
-              type="time"
-              label="Time"
-            />
-            <CheckboxExample />
-            <ButtonGroup variant="contained" color="primary">
-              <Button
-                startIcon={<SaveIcon />}
-                size="large"
-                style={{ fontSize: 20 }}
-                href="#"
-              >
-                Save
-              </Button>
-              <Button
-                startIcon={<DeleteIcon />}
-                size="large"
-                style={{ fontSize: 20, marginLeft: 10 }}
-                href="#"
-              >
-                Discard
-              </Button>
-            </ButtonGroup>
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          </header>
-        </div>
-      </Container>
+            <Grid item xs={3} sm={6}>
+              {/* i.e. card */}
+              <Paper style={{ height: 75, width: "100%" }} />
+            </Grid>
+            <Grid item xs={3} sm={6}>
+              {/* i.e. card */}
+              <Paper style={{ height: 75, width: "100%" }} />
+            </Grid>
+          </Grid>
+          <TextField
+            variant="outlined"
+            color="secondary"
+            placeholder="test@test.com"
+            label="email"
+          />
+          <TextField variant="filled" color="secondary" type="date" />
+          <TextField
+            variant="outlined"
+            color="secondary"
+            type="time"
+            label="Time"
+          />
+          <CheckboxExample />
+          <ButtonGroup variant="contained" color="primary">
+            <Button
+              startIcon={<SaveIcon />}
+              size="large"
+              style={{ fontSize: 20 }}
+              href="#"
+            >
+              Save
+            </Button>
+            <Button
+              startIcon={<DeleteIcon />}
+              size="large"
+              style={{ fontSize: 20, marginLeft: 10 }}
+              href="#"
+            >
+              Discard
+            </Button>
+          </ButtonGroup>
+        </header>
+      </div>
+      {/* </Container> */}
     </ThemeProvider>
   );
 }
